fc75ddb008fde4f8c563bdba386cf361bf97244e
talha-ahmar <talhaahmar391@gmail.com>
2026-02-25 22:36:53 +0500

    Iinitial commit

---
+++ MackBook_pro/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./componenets/Navbar.jsx"
+
+const App = () =>{
+
+
+    return(
+    <main>
+    <Navbar />
+    </main>
+    )
+}
+
+export default App
---
+++ MackBook_pro/src/constant/index.js
@@ -0,0 +1,10 @@
+export const navLinks = [
+  [
+    { label: "Store" },
+    { label: "About" },
+    { label: "iPhone" },
+    { label: "Watch" },
+    { label: "Vision" },
+    { label: "AirPods" },
+  ],
+];
